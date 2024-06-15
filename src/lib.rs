872d721c0b249093018e5884ca3576808f3e247d
Mitesh <singhmitesh19@gmail.com>
2024-06-15 23:45:27 +0530

    chore: Update is_hidden function to handle invalid file names and starts with dot in the begining

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-use std::path::{Path, PathBuf};
 use std::ffi::CString;
 use std::io;
+use std::path::{Path, PathBuf};
 
 #[cfg(target_os = "windows")]
 use winapi::um::fileapi::GetFileAttributesA;
 #[cfg(target_os = "windows")]
 use winapi::um::winnt::FILE_ATTRIBUTE_HIDDEN;
 
-#[cfg(target_os = "unix")]
-use std::os::unix::fs::MetadataExt;
 #[cfg(any(target_os = "macos", target_os = "ios", target_os = "freebsd"))]
 use libc::{stat, UF_HIDDEN};
+#[cfg(target_os = "unix")]
+use std::os::unix::fs::MetadataExt;
 
 /// Checks if the specified file is hidden.
 ///
@@ -24,6 +24,12 @@ use libc::{stat, UF_HIDDEN};
 /// * `Ok(false)` if the file is not hidden.
 /// * `Err` if there is an error accessing the file metadata.
 pub fn is_hidden(path: &PathBuf) -> io::Result<bool> {
+    let file_name = path
+        .file_name()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file name"))?;
+    if file_name.to_str().map_or(false, |s| s.starts_with('.')) {
+        return Ok(true);
+    }
     #[cfg(target_os = "windows")]
     {
         let path_c = CString::new(path.to_str().unwrap())?;
@@ -36,15 +42,6 @@ pub fn is_hidden(path: &PathBuf) -> io::Result<bool> {
 
     #[cfg(target_os = "macos")]
     {
-        // Check if the file name starts with a dot
-        let file_name = path
-            .file_name()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file name"))?;
-        if file_name.to_str().map_or(false, |s| s.starts_with('.')) {
-            return Ok(true);
-        }
-
-        // Check the UF_HIDDEN attribute
         let mut file_stat: stat = unsafe { std::mem::zeroed() };
         let path_c = CString::new(path.to_str().unwrap())?;
         let ret = unsafe { libc::stat(path_c.as_ptr(), &mut file_stat) };
@@ -57,10 +54,7 @@ pub fn is_hidden(path: &PathBuf) -> io::Result<bool> {
 
     #[cfg(target_os = "linux")]
     {
-        let file_name = path
-            .file_name()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file name"))?;
-        return Ok(file_name.to_str().map_or(false, |s| s.starts_with('.')));
+        // just check if the file name starts with a dot which is done above
     }
 
     #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
